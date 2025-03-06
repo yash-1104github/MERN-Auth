a9a3790c617facbaeb9defaafe25d247af0e503c
Yash <gyash1104@gmail.com>
2025-03-07 02:30:48 +0530

    update url

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -26,7 +26,9 @@ const Signup = () => {
             return handleError('All fields are required');
         }
         try {
-            const url = "http://localhost:8080/auth/signup";
+            // const url = "http://localhost:8080/auth/signup";
+            const url = `https://mern-auth-api-woad.vercel.app/auth/signup`;
+
             const response = await fetch(url, {
                 method: 'POST',
                 headers: {
